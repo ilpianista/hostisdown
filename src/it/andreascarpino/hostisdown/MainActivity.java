a907d43d23510d2ca070604627ee338b26cfb98e
Andrea Scarpino <me@andreascarpino.it>
2013-09-15 13:48:33 +0200

    Introduce the About menu

--- src/it/andreascarpino/hostisdown/MainActivity.java
+++ src/it/andreascarpino/hostisdown/MainActivity.java
@@ -1,12 +1,18 @@
 package it.andreascarpino.hostisdown;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import it.andreascarpino.hostisdown.task.PingTask;
 
 public class MainActivity extends Activity {
+
     /**
      * Called when the activity is first created.
      */
@@ -30,4 +36,39 @@ public class MainActivity extends Activity {
         String host = ((EditText) findViewById(R.id.host)).getText().toString();
         new PingTask(view.getRootView()).execute(host);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (R.id.about == item.getItemId()) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            try {
+                builder.setMessage(
+                        getString(R.string.app_name) + " " +
+                        this.getPackageManager().getPackageInfo(this.getPackageName(), 0).versionName +
+                        "\n\n" +
+                        "License: MIT\n" +
+                        "Copyright: 2013\n" +
+                        "Andrea Scarpino <me@andreascarpino.it>")
+                        .setTitle("About");
+            } catch (PackageManager.NameNotFoundException e) {
+                e.printStackTrace();
+            }
+
+            AlertDialog about = builder.create();
+            about.show();
+
+            return true;
+        }
+
+        return false;
+    }
+
 }
